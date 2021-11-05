2bdc1db84a399b345492a4e88252ffa40791b2bd
Snazzah <suggesttosnazzy@gmail.com>
2021-11-04 19:07:17 -0500

    chore: remove old file from cache

--- src/cron.ts
+++ src/cron.ts
@@ -1,5 +1,4 @@
 import { flushCache as flushSteamCache } from './interfaces/steam';
-import { flushCache as flushDDGCache } from './interfaces/ddg';
 import { flushCache as flushMCServerCache } from './interfaces/minecraft/server';
 import { flushCache as flushMCUserCache } from './interfaces/minecraft/user';
 import { CronJob } from 'cron';
@@ -9,7 +8,6 @@ function flushCaches() {
   flushSteamCache();
   flushMCServerCache();
   flushMCUserCache();
-  flushDDGCache();
   logger.debug('Flushed all caches.');
 }
 
